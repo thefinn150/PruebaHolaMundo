0ca912ae53dd6ec6522df4276edc01e2574183db
thefinn150 <xxthefinn150xx@gmail.com>
2022-02-18 12:43:56 -0600

    Mi primer commit

---
+++ src/prueba/hola/mundo/HolaMundo.java
@@ -0,0 +1,20 @@
+
+package prueba.hola.mundo;
+
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author 93616
+ */
+public class HolaMundo {
+
+
+
+public static void main(String[] args) {
+System.out.println("Hola Mundo");
+JOptionPane.showMessageDialog(null, "Hola Mundo");
+}
+
+}
